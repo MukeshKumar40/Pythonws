47529a820a2231a39d79d61011bd1759d56079c9
Mukesh Kumar <mukeshselfkd@gmail.com>
2024-09-25 23:01:14 +0530

    Add files via upload

---
+++ Basic.py
@@ -0,0 +1,9 @@
+print("Hello World")
+
+salary = 2500
+name = "Mukesh kumar"
+email = "mukeshselfkd@gmail.com"
+print(salary)
+print(name +" " +email,salary)
+
+print(F"My name is {name} and email is {email}")
